c77bf4555a690e52d78606887d9607f8029febfd
Taylor Dinwiddie <taylordinwiddie@gmail.com>
2019-10-22 22:51:01 -0700

    First Commit Test

---
+++ Colorblind/Assets/Scripts/ButtonClicker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClicker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Open");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
