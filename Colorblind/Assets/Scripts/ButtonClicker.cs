098d74b9c11dafcd6518af68573fb06ec12cc272
Taylor Dinwiddie <taylordinwiddie@gmail.com>
2019-10-23 14:41:15 -0700

    Fixed Raycast for Switches

--- Colorblind/Assets/Scripts/ButtonClicker.cs
+++ Colorblind/Assets/Scripts/ButtonClicker.cs
@@ -7,12 +7,28 @@ public class ButtonClicker : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Open");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        //Debug.Log("ray is " + ray);
+        if(Physics.Raycast(ray, out hit))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if(hit.transform.tag == "Button")
+                {
+                   Debug.Log("Hit" + hit.transform.gameObject.name);
+                   GameObject button = hit.transform.gameObject;
+                   
+                }
+                
+                
+            }
+        }
     }
 }
